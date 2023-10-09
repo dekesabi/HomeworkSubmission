7aa6115d7143109b27a15f51a646809ce0b49150
dekesabi <61382036+dekesabi@users.noreply.github.com>
2023-10-09 23:41:28 +0800

    Update README.md

---
+++ build/CMakeFiles/zuoyestatic.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zuoyestatic.dir/src/static_lib_src/static_lib.cpp.o"
+  "CMakeFiles/zuoyestatic.dir/src/static_lib_src/static_lib.cpp.o.d"
+  "libzuoyestatic.a"
+  "libzuoyestatic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zuoyestatic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build2/CMakeFiles/zuoyestatic.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zuoyestatic.dir/src/shared_lib_src/shared_lib.cpp.o"
+  "CMakeFiles/zuoyestatic.dir/src/shared_lib_src/shared_lib.cpp.o.d"
+  "libzuoyestatic.pdb"
+  "libzuoyestatic.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zuoyestatic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
