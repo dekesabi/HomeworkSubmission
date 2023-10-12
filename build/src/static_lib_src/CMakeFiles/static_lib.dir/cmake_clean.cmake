9073cc3c3a1ea835caffbba70ecbf98d4e2b3720
Edward <2799768454@qq.com>
2023-10-12 23:31:44 +0800

    Homework submission from jinjunyi

---
+++ build/src/static_lib_src/CMakeFiles/static_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../../lib/libstatic_lib.pdb"
+  "../../../lib/libstatic_lib.so"
+  "CMakeFiles/static_lib.dir/static_lib.cpp.o"
+  "CMakeFiles/static_lib.dir/static_lib.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/static_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
