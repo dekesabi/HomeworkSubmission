c7b7219245b2033008d01b487054e40839a9297e
ziluolan-akira <1093680880@qq.com>
2023-10-15 02:56:08 +0800

    还没搞清楚bin文件夹应该在哪个文件层级下，是否需要在build文件夹下？不过已经弄懂原理了这些都好修改

---
+++ src/static_lib_src/CMakeFiles/static_lib.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wqs/HW_homework/HW_homework2/src/static_lib_src/static_lib.cpp" "/home/wqs/HW_homework/HW_homework2/src/static_lib_src/CMakeFiles/static_lib.dir/static_lib.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "includes"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
