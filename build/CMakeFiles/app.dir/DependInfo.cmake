3b24fbfa04843a733fd1ed5cd8fe9e0bc6761107
ztf <1216170728@qq.com>
2023-10-15 18:41:30 +0800

    f

---
+++ build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/qixcrash/homework/HomeworkSubmission/main.cpp" "/home/qixcrash/homework/HomeworkSubmission/build/CMakeFiles/app.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../includes"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/qixcrash/homework/HomeworkSubmission/build/CMakeFiles/static_lib.dir/DependInfo.cmake"
+  "/home/qixcrash/homework/HomeworkSubmission/build/CMakeFiles/shared_lib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
