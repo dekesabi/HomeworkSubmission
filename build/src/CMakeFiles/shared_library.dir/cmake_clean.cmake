872bf31c9194367d7e4f8cf75325a1b3964532f7
llybyw <1205529342@qq.com>
2023-10-15 04:52:28 -0700

    111

---
+++ build/src/CMakeFiles/shared_library.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shared_library.dir/shared_lib.cpp.o"
+  "libshared_library.pdb"
+  "libshared_library.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shared_library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
